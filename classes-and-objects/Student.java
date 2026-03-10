20039dd5cbd2da2ee81a93fb793504eb73cd217c
Jasmin Ann M. Casapao <casapaojm@students.nu-laguna.edu.ph>
2026-03-10 14:02:22 +0000

    Product

--- classes-and-objects/Student.java
+++ classes-and-objects/Student.java
@@ -37,14 +37,14 @@ public class Student {
         totalStudents++;
     }
 
-    // Behavior without parameters
+    // Method without parameters
     void introduce() {
         System.out.println("Hi, I am " + name + ".");
         System.out.println("I am " + age + " years old.");
         System.out.println("My course is " + course + ".");
     }
 
-    // Behavior with parameters
+    // Method with parameters
     void updateCourse(String newCourse) {
         course = newCourse;
         System.out.println(name + " has updated their course to " + course + ".");
