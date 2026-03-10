5dca624988c2bb71e5d0a3e736763079dcca68c1
Kenneth Saan <kennethsaan16@gmail.com>
2026-03-10 22:40:25 +0800

    Add comments to ObjectDetailsPrinter for clarity

--- classes-and-objects/ObjectDetailsPrinter-Saan-Cantella.java
+++ classes-and-objects/ObjectDetailsPrinter-Saan-Cantella.java
@@ -1,22 +1,22 @@
 public class ObjectDetailsPrinter {
     public static void main(String[] args) {
         
-        Admin superAdmin = new Admin("JuanDev", "Pass123!", "juan@laguna.edu", true);
+        Admin superAdmin = new Admin("JuanDev", "Pass123!", "juan@laguna.edu", true); // // create an Admin using the constructor that sets all fields
+     
+        Admin moderator = new Admin("Maria_Mod", "SecureHash99"); // // create an Admin with only username/password (status defaults false)
         
-        Admin moderator = new Admin("Maria_Mod", "SecureHash99");
-        
-        Admin tempAdmin = new Admin();
+        Admin tempAdmin = new Admin(); // default Admin object (all fields default values)
 
-        System.out.println("--- Testing Admin Logins ---");
+        System.out.println("--- Testing Admin Logins ---"); // test login behavior for each admin
         superAdmin.login();
         moderator.login();
         
-        System.out.println("\n--- Displaying Admin Info ---");
+        System.out.println("\n--- Displaying Admin Info ---"); // print information for all objects
         superAdmin.displayInfo();
         moderator.displayInfo();
         tempAdmin.displayInfo();
 
-        System.out.println("\n--- Global Statistics ---");
+        System.out.println("\n--- Global Statistics ---");// show the static counter of created Admin objects
         Admin.printCounter();
     }
 }
