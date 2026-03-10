06612cef29705b05a3ffb52ef3670863dd5198d9
Baguette <tismoye@students.nu-laguna.edu.ph>
2026-03-10 13:04:41 +0000

    add comments

--- classes-and-objects/yarn.java
+++ classes-and-objects/yarn.java
@@ -1,10 +1,13 @@
 public class yarn {
+    // Attributes
     String color;
     int plysize;
     int grams;
 
+    // Static attribute
     static int totalYarn = 0;
 
+    // Default constructor
     yarn(){
         color = "white";
         plysize = 4;
@@ -12,6 +15,7 @@ public class yarn {
         totalYarn++;
     }
 
+    // Parameterized constructor
     yarn(String color, int plysize, int grams) {
         this.color = color;
         this.plysize = plysize;
@@ -19,25 +23,29 @@ public class yarn {
         totalYarn++;
     }
 
+    // Overloaded constructor
     yarn(String color, int plysize) {
         this.color = color;
         this.plysize = plysize;
-        this.grams = 20; // default age
+        this.grams = 20; // default grams
         totalYarn++;
     }
 
+    // Behavior without parameters
     void order() {
         System.out.println("I would like the color " + color + ".");
         System.out.println("The ply size should be " + plysize + " ply.");
         System.out.println("The grams should be " + grams + ".");
     }
 
+    // Behavior with parameters
     void updateGrams(int newGrams) {
         grams = newGrams;
         System.out.println("Buyer has updated the yarn grams to " + grams + ".");
     }
 
+    // Static method
     static void displayTotalOrders() {
-        System.out.println("Total yarn orders submitted: " + totalYarn);
+        System.out.println("Total yarn orders: " + totalYarn);
     }
 }
