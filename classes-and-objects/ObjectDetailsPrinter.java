554c25cc7c746c57999bc278a0bdf5b134628a1c
Kenneth Saan <kennethsaan16@gmail.com>
2026-03-10 14:18:19 +0000

    Added Product class

--- classes-and-objects/ObjectDetailsPrinter.java
+++ classes-and-objects/ObjectDetailsPrinter.java
@@ -1,27 +1,23 @@
 public class ObjectDetailsPrinter {
     public static void main(String[] args) {
-        // This is the object instance created from the Student class
-        // Object using default constructor
-        Student s1 = new Student();
-
-        // Object using parameterized constructor
-        Student s2 = new Student("Maria", 20, "BSCS");
-
-        // Object using overloaded constructor
-        Student s3 = new Student("Carlos", "BSIT");
-
-        // Print attributes and trigger behaviors
-        s1.introduce();
-        s2.introduce();
-
-        // Method with parameter
-        s2.updateCourse("BS Data Science");
-
-        // Static method
-        Student.displayTotalStudents();
         
+        Admin superAdmin = new Admin("JuanDev", "Pass123!", "juan@laguna.edu", true);
+        
+        Admin moderator = new Admin("Maria_Mod", "SecureHash99");
+        
+        Admin tempAdmin = new Admin();
+
+        System.out.println("--- Testing Admin Logins ---");
+        superAdmin.login();
+        moderator.login();
+        
+        System.out.println("\n--- Displaying Admin Info ---");
+        superAdmin.displayInfo();
+        moderator.displayInfo();
+        tempAdmin.displayInfo();
+
+        System.out.println("\n--- Global Statistics ---");
+        Admin.printCounter();
     }
 }
 
-// javac classes-and-objects-lab/Student.java classes-and-objects-lab/ObjectDetailsPrinter.java
-// java -cp classes-and-objects-lab ObjectDetailsPrinter
