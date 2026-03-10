03964baf7bdd613a5c17d0f0944227ffd445da10
Vinz Joseph Silva <vinzjosephsilva@gmail.com>
2026-03-10 14:51:28 +0000

    Created Accounts Class

--- classes-and-objects/Accounts.java
+++ classes-and-objects/Accounts.java
@@ -1,42 +1,48 @@
-Class Accounts{
-    Static String Account
-
+public class Accounts{
+    
     String AccountName;
     String AccountOwner;
     String AccountType;
-}
+    static int TotalAccount = 0;
+
 
 Accounts(){
-    String AccountName = "Unknown";
-    String AccountOwner = "Unknown";
-    String AccountType = "Unknown";
-    
+    AccountName = "Unknown";
+    AccountOwner = "Unknown";
+    AccountType = "Unknown";
+    TotalAccount++;
 }
 
 Accounts (String AccountName, String AccountOwner, String AccountType){
     this.AccountName = AccountName;
     this.AccountOwner = AccountOwner;
     this.AccountType = AccountType;
+    TotalAccount++;
 }
 
 Accounts (String AccountName, String AccountOwner){
     this.AccountName = AccountName;
     this.AccountOwner = AccountOwner;
     this.AccountType = "NU Account";
+    TotalAccount++;
 }
 
-Void AccountDisplay(){
+void AccountDisplay(){
     System.out.println ("Account Name: " + AccountName);
     System.out.println ("Name of the Owner: " + AccountOwner);
     System.out.println ("Account Type: " + AccountType);
 }
 
-Void AccountAdd (String AddedAccount){
-    Add = AddedAccount
+void AccountAdd (String AddedAccount){
+    Add = AddedAccount;
     System.out.println("Your account " + Add +  "has been added ");
 }
 
+static void displayTotalAccount(){
+    System.out.println("Total Accounts: " + TotalAccount);
+}
 
+}
 
 
 
