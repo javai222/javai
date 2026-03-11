df188adb0136dbdba9bbc63bad84b95522acfe48
grimrex-dev <charlesjustinjavier@gmail.com>
2026-03-11 02:54:27 +0000

    object created

--- classes-and-objects/ObjectDetailsPrinter.java
+++ classes-and-objects/ObjectDetailsPrinter.java
@@ -1,6 +1,23 @@
 public class ObjectDetailsPrinter {
     public static void main(String[] args) {
-        // static method
+        // This is the object instance created from the Student class
+        // Object using default constructor
+        Student s1 = new Student();
+
+        // Object using parameterized constructor
+        Student s2 = new Student("Maria", 20, "BSCS");
+
+        // Object using overloaded constructor
+        Student s3 = new Student("Carlos", "BSIT");
+
+        // Print attributes and trigger behaviors
+        s1.introduce();
+        s2.introduce();
+
+        // Method with parameter
+        s2.updateCourse("BS Data Science");
+
+        // Static method
         Student.displayTotalStudents();
         
     }
