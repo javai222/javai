4bd1c6582b30badc5386e1c81304e56c58d30725
Vinz Joseph Silva <vinzjosephsilva@gmail.com>
2026-03-11 02:55:17 +0000

    Updated comments

--- classes-and-objects/Accounts.java
+++ classes-and-objects/Accounts.java
@@ -1,45 +1,53 @@
+/*
+ * This class represents an account with a name, owner, and type.
+ * It also keeps track of the total number of accounts created.
+ * Created by Vinz Silva
+ */
+
 public class Accounts{
-    
+    //attributes
     String AccountName;
     String AccountOwner;
     String AccountType;
     static int TotalAccount = 0;
 
-
+    //default constructor
 Accounts(){
     AccountName = "Unknown";
     AccountOwner = "Unknown";
     AccountType = "Unknown";
     TotalAccount++;
 }
-
+    // parameterized constructor
 Accounts (String AccountName, String AccountOwner, String AccountType){
     this.AccountName = AccountName;
     this.AccountOwner = AccountOwner;
     this.AccountType = AccountType;
     TotalAccount++;
 }
-
+    // overloaded constructor
 Accounts (String AccountName, String AccountOwner){
     this.AccountName = AccountName;
     this.AccountOwner = AccountOwner;
     this.AccountType = "NU Account";
     TotalAccount++;
 }
-
+    // behavior without parameters
 void AccountDisplay(){
     System.out.println ("Account Name: " + AccountName);
     System.out.println ("Name of the Owner: " + AccountOwner);
     System.out.println ("Account Type: " + AccountType);
 }
-
+    // behavior with parameters
 void AccountAdd (String AddedAccount){
     AccountName = AddedAccount;
     System.out.println("Your account " + AccountName +  "has been added ");
 }
-
+    //static method
 static void displayTotalAccount(){
     System.out.println("Total Accounts: " + TotalAccount);
 }
-
 }
+
+
+
