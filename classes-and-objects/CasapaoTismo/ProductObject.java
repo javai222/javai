43fde0bc0290d6f4794f18e4943c1b6de58c7fbe
Baguette <tismoye@students.nu-laguna.edu.ph>
2026-03-11 05:24:58 +0000

    Object Creation

---
+++ classes-and-objects/CasapaoTismo/ProductObject.java
@@ -0,0 +1,34 @@
+//Object created by Tismo, Class created by casapao
+
+public class ProductObject {
+    public static void main(String[] args) {
+
+        // Object using default constructor
+        Product p1 = new Product();
+
+        // Object using parameterized constructor
+        Product p2 = new Product("Laptop", 50000);
+
+        // Object using overloaded constructor
+        Product p3 = new Product("Phone", 20000, "Samsung");
+
+        // Display product details
+        p1.displayProduct();
+        System.out.println();
+
+        p2.displayProduct();
+        System.out.println();
+
+        p3.displayProduct();
+        System.out.println();
+
+        // Change price using method with parameter
+        p2.setPrice(48000);
+        System.out.println("Updated Price for Laptop:");
+        p2.displayProduct();
+        System.out.println();
+
+        // Call static method
+        Product.printTotalObjects();
+    }
+}
