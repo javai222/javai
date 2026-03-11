b43e2264b2deb5889bc20927a7e40f14d95fc083
Ryan James M. Balanon <balanonrm@students.nu-laguna.edu.ph>
2026-03-11 00:52:29 +0000

    Created car class

---
+++ classes-and-objects/Cars.java
@@ -0,0 +1,58 @@
+public class Car {
+
+    // Attributes that describe a car
+    String brand;
+    String model;
+    double price;
+
+    // Static variable to count how many car objects are created
+    static int totalCars = 0;
+
+    // Default constructor (no parameters)
+    // Sets default values when a car object is created
+    Car() {
+        brand = "Unknown Brand";
+        model = "Unknown Model";
+        price = 0.0;
+        totalCars++; // increase the number of cars created
+    }
+
+    // Parameterized constructor
+    // Allows the user to set values when creating the object
+    Car(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        totalCars++; // increase the counter
+    }
+
+    // Overloaded constructor
+    // Only the brand is provided
+    Car(String brand) {
+        this.brand = brand;
+        model = "Unknown Model";
+        price = 0.0;
+        totalCars++; // increase the counter
+    }
+
+    // Method without parameters
+    // Displays the details of the car
+    void displayCar() {
+        System.out.println("Car Brand: " + brand);
+        System.out.println("Car Model: " + model);
+        System.out.println("Car Price: " + price);
+    }
+
+    // Method with parameter
+    // Updates the price of the car
+    void setPrice(double newPrice) {
+        price = newPrice;
+        System.out.println("New price set to: " + price);
+    }
+
+    // Static method
+    // Displays the total number of car objects created
+    static void printTotalCars() {
+        System.out.println("Total Cars Created: " + totalCars);
+    }
+}
