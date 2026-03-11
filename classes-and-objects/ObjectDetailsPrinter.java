e905dca1a7746e3229fae2136738fa1effa3b481
Henry <Henrydolormente11706@gmail.com>
2026-03-11 10:12:04 +0800

    Refactor ObjectDetailsPrinter for improved clarity
    Refactor ObjectDetailsPrinter to utilize constructors and methods for Student and Studentinfo classes.

--- classes-and-objects/ObjectDetailsPrinter.java
+++ classes-and-objects/ObjectDetailsPrinter.java
@@ -1,36 +1,25 @@
 public class ObjectDetailsPrinter {
     public static void main(String[] args) {
-        // static method
+        // This is the object instance created from the Student class
+        // Object using default constructor
+        Student s1 = new Student();
+
+        // Object using parameterized constructor
+        Student s2 = new Student("Maria", 20, "BSCS");
+
+        // Object using overloaded constructor
+        Student s3 = new Student("Carlos", "BSIT");
+
+        // Print attributes and trigger behaviors
+        s1.introduce();
+        s2.introduce();
+
+        // Method with parameter
+        s2.updateCourse("BS Data Science");
+
+        // Static method
         Student.displayTotalStudents();
         
-
-        //Object created (Cantela), Class created (Saan)
-        // This is the object instance created from the Studentinfo class
-        System.out.println("=====================================");
-        System.out.println("      Student Information System");
-        System.out.println("=====================================\n");
-
-        //Default Constructor
-        Studentinfo student1 = new Studentinfo();
-        student1.display();
-        student1.StudentVerify();
-        System.out.println();
-
-        //Parameterized Constructor (Name, Number, Grade)
-        Studentinfo student2 = new Studentinfo("Maria Santos", 1001, 10);
-        student2.display();
-        student2.StudentVerify();
-        System.out.println();
-
-        // Test 3: Overloaded Constructor (Name, Number)
-        Studentinfo student3 = new Studentinfo("Juan Dela Cruz", 1002);
-        student3.display();
-        student3.StudentVerify();
-        System.out.println();
-
-        // Display total number of students created
-        System.out.println("Display Total Students");
-        Studentinfo.showTotal();
     }
 }
 
