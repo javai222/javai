fde3557230aaa2b89ddc7dd3cc6e12bcc2a84f0e
Rahzel Heeroe R. Conel <reyesconel@gmail.com>
2026-03-11 04:01:06 +0000

    mouseSales

---
+++ classes-and-objects/mouse.java
@@ -0,0 +1,42 @@
+public class mouse {
+
+String brand;
+String color;
+String type;
+
+public static int mouseSales = 0; 
+
+public mouse () { //no parameters
+this.brand = "razer";
+this.color = "black";
+this.typo = "wireless";
+
+mouseSales++;
+}
+
+public mouse (String b, String c, String t) { //with parameters
+this.brand = b
+this.color = c
+this.typo = t
+
+mouseSales++;
+}
+public mouse (String c, String t) { //with parameter overload
+this.brand = "razer";
+this.color = c
+this.typo = t
+
+mouseSales++;
+}
+   //Behavior without parameters
+   void displayItems(){
+        System.out.println("Mouse Brand: " +brand);
+        System.out.println("Mouse Color: " +color);
+        System.out.println("Mouse Type: " +type);
+   }
+
+   //Static method
+        static void displayTotalSales(){
+            System.out.println("Total Items: " mouseSales);
+        }
+}
