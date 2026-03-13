67d1a7b16013d475aaee1faf3de29aac9e6d8e99
Baguette <tismoye@students.nu-laguna.edu.ph>
2026-03-13 10:43:54 +0000

    Created yarn encapsulated class

--- encapsulation-and-access-modifiers-lab/Yarn.java
+++ encapsulation-and-access-modifiers-lab/Yarn.java
@@ -1,73 +1,84 @@
 public class Yarn {
     /*
-    *This class represents yarn orders with colors, ply size, and grams
-    *Created by Yvette Germaine Tismo
-    */
-    
-    //Private attributes of the yarn
-    private String color;
-    private String yarnType;
-    private int plySize;
-    private int grams;
-
+	*This class represents yarn orders with colors, ply size, and grams
+	*Created by Yvette Germaine Tismo
+	*/
+	    
+	//Private attributes of the yarn
+	private String color;
+	private String yarnType;
+	private int plySize;//must be no less than 4 but not greater than 16 grams
+	private int grams; //must be no less than 20 grams but not greater than 200 grams
+	    
     // Static attribute
-    private static int totalYarn = 0;
+	private static int totalYarn = 0;
 
-    // Default constructor
+	// Default constructor
     public Yarn(){
-        color = "white";
-        yarnType = "Acrylic";
-        plySize = 4;
-        grams = 20;
-        totalYarn++;
-    }
+	    color = "white";
+	    yarnType = "Acrylic";
+	    plySize = 4;
+	    grams = 20;
+	    totalYarn++;
+	}
 
-    // Overloaded constructor
+	// Overloaded constructor
     public Yarn(String color, String yarnType, int plySize) {
-        this.color = color;
-        this.yarnType = yarnType;
-        this.plySize = plySize;
-        this.grams = 20; // default grams
-        totalYarn++;
+	    this.color = color;
+	    this.yarnType = yarnType;
+	    this.plySize = plySize;
+	    this.grams = 20; // default grams
+	    totalYarn++;
     }
 
-    // Parameterized constructor
-    public Yarn(String color, String yarnType, int plySize, int grams) {
-        this.color = color;
-        this.yarnType = yarnType;
-        this.plySize = plySize;
-        this.grams = grams;
-        totalYarn++;
+	// Parameterized constructor
+	public Yarn(String color, String yarnType, int plySize, int grams) {
+	    this.color = color;
+	    this.yarnType = yarnType;
+	    this.plySize = plySize;
+	    this.grams = grams;
+	    totalYarn++;
     }
 
-    public String getColor() { return this.color; }
+	public String getColor() { return this.color; }
 
-    public String getYarnType() { return this.yarnType; }
+	public String getYarnType() { return this.yarnType; }
 
-    public int getPlysize(){ return this.plySize; }
+	public int getPlysize(){ return this.plySize; }
 
-    public int getGrams(){ return this.grams; }
+	public int getGrams(){ return this.grams; }
 
-    // Behavior with parameters
-    public void setColor(String color) {
-        if (color != ""){
-            this.color = color;
-            System.out.println("Buyer has updated the yarn color to " + color + ".");
-        }
-    }
+	    
+	// Behavior with parameters
+	public void setColor(String color) {
+	    //validation
+	    if (color != "")
+	        this.color = color;
+	}
 
-    public void setYarnType(String yarnType) {
-        if (yarnType != "")
-            this.yarnType = yarnType;
-    }
+	public void setYarnType(String yarnType) {
+	    //validation
+	    if (yarnType != "")
+	        this.yarnType = yarnType;
+	}
 
-    public void setPlysize(int newPlysize) {
-        if (plySize >= 4 && plySize <= 16)
-            this.plySize = newPlysize;
-    }
+	public void setPlysize(int newPlysize) {
+	    //Object Invariant  ply size should be at least 4 but not greater than 16
+	    if (plySize >= 4 && plySize <= 16) {
+	        this.plySize = newPlysize;
+	    } else 
+	    	System.out.println("Invalid Input.");
+	}
 
     public void setGrams(int newGrams) {
-        if (grams > 0 && grams <= 200)
-            this.grams = newGrams;
-    }
+	    //Object Invariant  grams should be at least 20 but not greater than 200
+	    if (grams >= 20 && grams <= 200) {
+	        this.grams = newGrams;
+	    } else 
+	        System.out.println("Invalid Input.");
+	}
+	    
+	public static void displayTotalOrders() {
+	    System.out.println("Total yarn orders: " + totalYarn);
+	}
 }
