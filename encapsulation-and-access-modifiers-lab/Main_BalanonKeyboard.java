f424ff3ec76c0d335a547cdd7ca55de2d3f0a991
Ryan James M. Balanon <balanonrm@students.nu-laguna.edu.ph>
2026-03-13 13:36:44 +0000

    Added obj to Keyboard class

---
+++ encapsulation-and-access-modifiers-lab/Main_BalanonKeyboard.java
@@ -0,0 +1,29 @@
+public class Main_BalanonKeyboard {
+    public static void main(String[] args) {
+
+        // Object using default constructor
+        Keyboard kb1 = new Keyboard();
+
+        // Object using constructor with parameters
+        Keyboard kb2 = new Keyboard("AttackShark", "Black", "Mechanical", 15);
+
+        // Object using overloaded constructor
+        Keyboard kb3 = new Keyboard("Brown", "Mechanical");
+
+        // Display keyboard information
+        kb1.displayItems();
+        System.out.println();
+
+        kb2.displayItems();
+        System.out.println();
+
+        kb3.displayItems();
+        System.out.println();
+
+        // Example behavior
+        kb1.sellKeyboard(2);
+
+        // Display total keyboards created
+        Keyboard.displayTotalSales();
+    }
+}
