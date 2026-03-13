0ec6184ec6fc2d90ec562e533d750739b5bb13df
Fayemaya <tjandroque@gmail.com>
2026-03-13 11:41:23 +0000

    NewMouseAndValoObj

--- encapsulation-and-access-modifiers-lab/Main_Mance.java
+++ encapsulation-and-access-modifiers-lab/Main_Mance.java
@@ -47,5 +47,38 @@ public class Main_Mance {
         Mouse.displayTotalSales();
 
         System.out.println("\n=== Transaction Completed ===");
+
+         // Creating objects
+        ValorantAccount acc1 = new ValorantAccount();
+        ValorantAccount acc2 = new ValorantAccount("John", "JRoque#123", "mypassword", 500, 10, "Gold", true);
+
+        // Using setters (valid values)
+        acc1.setUserName("Rahzel");
+        acc1.setValorantID("Rahzel#777");
+        acc1.setPassWord("secret123");
+        acc1.setValorantPoints(300);
+
+        // Demonstrating validation rules (invalid inputs)
+        acc1.setUserName("");          // Invalid username
+        acc1.setPassWord("123");       // Invalid password (less than 6)
+        acc1.setValorantPoints(-50);   // Invalid points
+
+        // Calling behaviors (getters)
+        System.out.println("Account 1 Information:");
+        System.out.println("Username: " + acc1.getUserName());
+        System.out.println("Valorant ID: " + acc1.getValorantID());
+        System.out.println("Valorant Points: " + acc1.getValorantPoints());
+
+        System.out.println();
+
+        System.out.println("Account 2 Information:");
+        System.out.println("Username: " + acc2.getUserName());
+        System.out.println("Valorant ID: " + acc2.getValorantID());
+        System.out.println("Valorant Points: " + acc2.getValorantPoints());
+
+        // Using static behavior
+        System.out.println();
+        System.out.println("Total Valorant Accounts Created: " + ValorantAccount.getAccountCount());
+        
     }
 }
