9dc10050a75973ef2a22e6ace9b1c4fbc57c150c
grimrex-dev <charlesjustinjavier@gmail.com>
2026-03-18 05:33:25 +0000

    added comments

--- inheritance-lab/animal.java
+++ inheritance-lab/animal.java
@@ -1,21 +1,34 @@
+/*
+* This class represents an animal parent class
+* Created by Charles Justin Javier
+*/
+
 public class animal {
+    
+    // Attributes (variables) that describe the animal
     private String name;
     private String classification;
     private String type;
 
-    animal(String name, String classification, String type) {
-        this.name = name;
-        this.classification = classification;
-        this.type = type;
+    // Constructor - used to initialize the object when created
+    public animal(String name, String classification, String type) {
+        this.name = name;               // sets the animal's name
+        this.classification = classification; // sets classification (e.g., Mammal, Reptile)
+        this.type = type;               // sets type (e.g., Pet, Wild)
     }
 
-    void play() {
-        System.out.print(name +" is playing.");
+    // Method that shows the animal playing
+    public void play() {
+        System.out.println(name + " is playing.");
     }
-    void growl() {
-        System.out.print(name +" is growling.");
+
+    // Method that shows the animal making a growling sound
+    public void growl() {
+        System.out.println(name + " is growling.");
     }
-    void sick() {
-        System.out.print("Animal not feeling well.");
+
+    // Method that can be overridden in child classes
+    public void sick() {
+        System.out.println("Animal is not feeling well.");
     }
 }
