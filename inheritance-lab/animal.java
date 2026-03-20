ebafd4679881f39e2ebe69d98407744b4c1e24be
grimrex-dev <charlesjustinjavier@gmail.com>
2026-03-20 10:19:34 +0000

    added parameterized constructors, setters, and getters

--- inheritance-lab/animal.java
+++ inheritance-lab/animal.java
@@ -10,11 +10,42 @@ public class animal {
     private String classification;
     private String type;
 
-    // Constructor - used to initialize the object when created
+    // Default Constructors
     public animal(String name, String classification, String type) {
-        this.name = name;               // sets the animal's name
-        this.classification = classification; // sets classification (e.g., Mammal, Reptile)
-        this.type = type;               // sets type (e.g., Pet, Wild)
+        this.name = name;              
+        this.classification = classification; 
+        this.type = type;               
+    }
+    // Parameterized Constructors
+     public animal(String name, String classification, String type) {
+        this.name = name;
+        this.classification = classification;
+        this.type = type;
+    }
+
+    //Getters
+     public String getName() {
+        return name;
+    }
+    public String getClassification() {
+        return classification;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     // Method that shows the animal playing
