4e087e450a1fa359099f24607e85dfc8b44d10c4
Henry <Henrydolormente11706@gmail.com>
2026-03-22 21:19:05 +0800

    Update MainInheritance_Dolormente.java

--- inheritance-lab/MainInheritance_Dolormente.java
+++ inheritance-lab/MainInheritance_Dolormente.java
@@ -7,9 +7,9 @@ public class MainInhertiance_Dolormente {
     
     instrument gt = new Guitar("ZVX Electric Guitar", "Electric guitar", "Hardwood");  // This line of code shows the IS-A relationship because guitar is an instrument
 
-    System.out.println("The guitar's name is " + getName() + ".");                     // Inherited from parent class
-    System.out.println("It's a " + getType() + " as its name suggests.");              // Inherited from parent class
-    System.out.println("Made from high quality " + getMaterial()                       // Inherited from parent class
+    System.out.println("The guitar's name is " + gt.getName() + ".");                     // Inherited from parent class
+    System.out.println("It's a " + gt.getType() + " as its name suggests.");              // Inherited from parent class
+    System.out.println("Made from high quality " + gt.getMaterial()                       // Inherited from parent class
                        + " that you would mostly see on quality electric guitars.");
     
     gt.clean();
@@ -19,10 +19,10 @@ public class MainInhertiance_Dolormente {
 
     instrument vl = new Violin("Vean Vio", "Modern Acoustic", "Tonewoods");            // This line of code shows the IS-A relationship because violin is an instrument
     
-    System.out.println("The violin's name is " + getName() + ".");                     // Get is inherited from parent class
-    System.out.println("It's a " + getType() + " type of violin.");                    // Get is inherited from parent class
+    System.out.println("The violin's name is " + vl.getName() + ".");                     // Get is inherited from parent class
+    System.out.println("It's a " + vl.getType() + " type of violin.");                    // Get is inherited from parent class
     System.out.println("Originated from Honduras, it is the most superior "            // Get is nherited from parent class
-                       + getMaterial() + " out of all the tonewoods in the world.");
+                       + vl.getMaterial() + " out of all the tonewoods in the world.");
 
     vl.play();                                                                         // Inherited from parent class
     vl.tune();                                                                         // Inherited from parent class
