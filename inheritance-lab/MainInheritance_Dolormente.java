ab802c949e55b7fd12b563b3a3c34f46c0872552
Henry <Henrydolormente11706@gmail.com>
2026-03-22 22:30:19 +0800

    Update MainInheritance_Dolormente.java

--- inheritance-lab/MainInheritance_Dolormente.java
+++ inheritance-lab/MainInheritance_Dolormente.java
@@ -22,37 +22,39 @@ public class MainInheritance_Dolormente {
     
     instrument gt = new Guitar("ZVX Electric Guitar", "Electric guitar", "Hardwood");     // This line of code shows the IS-A relationship because guitar is an instrument
    
-    System.out.println("= = = = = Guitar Details = = = = =");
+    System.out.println("= = = = = = = Guitar Details = = = = = = =");
     System.out.println("The guitar's name is " + gt.getName() + ".");                     // Inherited from parent class
     System.out.println("It's an " + gt.getType() + " as its name suggests.");              // Inherited from parent class
     System.out.println("Made from high quality " + gt.getMaterial()                       // Inherited from parent class
                        + " that you would mostly see on quality electric guitars.");
-    System.out.println("= = = = = = = = = = = = = = =\n");
+    System.out.println("______________________________________________________________________________________\n");
 
-    System.out.println("= = = = = Fine tuning = = = = =");
+    System.out.println("= = = = = = = Fine tuning = = = = = = =");
     gt.clean();                                                                           // Inherited from parent class
     gt.tune();                                                                            // Inherited from parent class
     gt.play();                                                                            // Inherited from parent class
-    gt.sound();                                                                           // Inherited from parent class
-    System.out.println("= = = = = = = = = = = = = = =\n");
     Guitar.playChord();                                                                   // New behavior from child class
+    gt.sound();                                                                           // Inherited from parent class
+    System.out.println("______________________________________________________________________________________\n");
+
 
     instrument vl = new Violin("Vean Vio", "Modern acoustic", "Tonewoods");               // This line of code shows the IS-A relationship because violin is an instrument
 
-    System.out.println("= = = = = Violin Details = = = = =");
+    System.out.println("= = = = = = = Violin Details = = = = = = =");
     System.out.println("The violin's name is " + vl.getName() + ".");                     // Get is inherited from parent class
     System.out.println("It's a " + vl.getType() + " type of violin.");                    // Get is inherited from parent class
     System.out.println("Originated from Honduras, it is the most superior "               // Get is inherited from parent class
                        + vl.getMaterial() + " out of all the tonewoods in the world.");
-    System.out.println("= = = = = = = = = = = = = = =\n");
+    System.out.println("______________________________________________________________________________________\n");
 
     System.out.println("= = = = = Fine tuning = = = = =");
     vl.clean();                                                                           // Inherited from parent class
     vl.tune();                                                                            // Inherited from parent class
     vl.play();                                                                            // Inherited from parent class
+    Violin.useBow();                                                                      // New behavior from child class   
     vl.sound();;                                                                          // Inherited from parent class
-    System.out.println("= = = = = = = = = = = = = = =\n");
-    Violin.useBow();                                                                      // New behavior from child class    
+    System.out.println("______________________________________________________________________________________\n");
+ 
   }
 }
     
