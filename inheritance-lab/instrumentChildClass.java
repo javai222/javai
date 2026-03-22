0a800aef03cc686b06615d9d7086b7f48f5cfa98
Henry <Henrydolormente11706@gmail.com>
2026-03-22 20:46:14 +0800

    Update instrumentChildClass.java

--- inheritance-lab/instrumentChildClass.java
+++ inheritance-lab/instrumentChildClass.java
@@ -8,7 +8,7 @@ class Guitar extends instrument {                                               
       super("ZVX Electric Guitar", "Electric guitar", "Hardwood");                          
   }
 
-  public static voidplayChord() {
+  public static void playChord() {
       System.out.println("Strumming a chord on the guitar.");                                 // New behavior created
 }
   
@@ -24,7 +24,7 @@ class Violin extends instrument {                                               
       super("Vean Vio", "Modern Acoustic", "Tonewoods");
   }
 
-  public static voiduseBow() {                                                                // New behavior created
+  public static void useBow() {                                                                // New behavior created
       System.out.println("Using the bow to play the violin smoothly.");
 }
   
