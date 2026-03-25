d6c57dbd3232374670fc891f213cfbfc79fdc440
Fayemaya <tjandroque@gmail.com>
2026-03-25 01:41:21 +0000

    mouseChildClass

--- inheritance-lab/attackShark.java
+++ inheritance-lab/attackShark.java
@@ -1,19 +1,19 @@
 // Child class that inherits from inheritanceMouse
-public class razer extends inheritanceMouse {
+public class attackShark extends inheritanceMouse {
 
     // Additional attributes
     private boolean hasRGB;
     private int buttonCount;
 
     // Default constructor
-    public razer() {
+    public attackShark() {
         super(); // calls parent default constructor
         this.hasRGB = false;
         this.buttonCount = 0;
     }
 
     // Parameterized constructor
-    public razer(String brand, String color, String type, int dpi, boolean hasRGB, int buttonCount) {
+    public attackShark(String brand, String color, String type, int dpi, boolean hasRGB, int buttonCount) {
         super(brand, color, type, dpi); // calls parent constructor
         this.hasRGB = hasRGB;
         this.buttonCount = buttonCount;
