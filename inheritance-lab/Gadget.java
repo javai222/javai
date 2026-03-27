11250ed04fb8645edec022842a84b4bfc4963263
v1nncy <torrefrancavp@students.nu-laguna.edu.ph>
2026-03-27 13:08:04 +0800

    Update Gadget.java

--- inheritance-lab/Gadget.java
+++ inheritance-lab/Gadget.java
@@ -1,40 +1,64 @@
-class Gadget {
-    // This class is made by Torrefranca
-    // Attributes
+public class Gadget {
+    // Parent class made bt Torrefranca
+    // Attributes that describe the gadget
     private String brand;
     private String model;
+    private String type;
     private int batteryLevel;
+    private String specialFeature;
 
-    // Default constructor
-    public Gadget() {
-        brand = "Unknown";
-        model = "Unknown";
-        batteryLevel = 0;
-    }
-
-    // Parameterized constructor
-    public Gadget(String brand, String model, int batteryLevel) {
+    // Constructor for initialization
+    public Gadget(String brand, String model, String type, int batteryLevel, String specialFeature) {
         this.brand = brand;
         this.model = model;
+        this.type = type;
         this.batteryLevel = batteryLevel;
+        this.specialFeature = specialFeature;
     }
 
-    // Behaviors
-    public void powerOn() {
-        System.out.println("Gadget is powering on");
-    }
-
-    public void powerOff() {
-        System.out.println("Gadget is shutting down");
-    }
-
-    // Method that can be overridden
-    public void use() {
-        System.out.println("Gadget is being used");
-    }
-
-    // Getter
+    // Getter methods
     public String getBrand() {
         return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    public String getSpecialFeature() {
+        return specialFeature;
+    }
+
+    // Displays the brand
+    public void showBrand() {
+        System.out.println("Brand: " + brand);
+    }
+
+    // Displays the model
+    public void displayModel() {
+        System.out.println("Model: " + model);
+    }
+
+    // Shows the type of gadget
+    public void showType() {
+        System.out.println("Type: " + type);
+    }
+
+    // Shows battery level
+    public void showBattery() {
+        System.out.println("Battery Level: " + batteryLevel + "%");
+    }
+
+    // This method can be overridden in child class
+    public void useGadget() {
+        System.out.println("Gadget is being used");
     }
 }
