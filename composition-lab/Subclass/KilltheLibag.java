09f97e651f1f23c60640ecd24099ed29f6ea94af
Vinz Joseph Silva <vinzjosephsilva@gmail.com>
2026-03-27 13:40:41 +0000

    Created subclass and demo

---
+++ composition-lab/Subclass/KilltheLibag.java
@@ -0,0 +1,72 @@
+/*
+* This class represents composed class
+* Created by Vinz Joseph R. Silva
+* Used classes of Capunpon (libag), Plaantilla (Water), Brush (De Leon)
+*/
+
+public class KilltheLibag {
+
+    // Attributes 
+    private Water water;
+    private Brush brush;
+    private Libag libag;
+
+    // Constructors
+public KilltheLibag (Water water, Brush brush, Libag libag){
+    this.water = water;
+    this.brush = brush;
+    this.libag = libag;
+}
+
+    // Getter methods 
+public Water getWater () {
+    return water;
+}
+
+public Brush getBrush () {
+    return brush;
+}
+
+public Libag getLibag () {
+    return libag;
+}
+
+    //Setter method
+public void setWater (Water water){
+    this.water = water;
+}
+
+public void setBrush (Brush brush){
+    this.brush = brush;
+}
+
+public void setLibag (Libag libag){
+    this.libag = libag;
+}
+
+    // behaviors (actions)
+
+public void introduceParts () {
+    water.display(); // will display the traits of water that will be used in fighting libag
+    brush.describe(); // will showcase the capacity of the brush to obliterate any single libag
+    libag.describe(); // will exhibit the characteristics of libag in this manner
+}
+
+public void startCleaning () {
+    water.pour(); // starts pouring the water to cleanse the libag area
+    libag.soak(); // the libag will soak and the thickness will melt slowly
+    brush.wearOut(); // We cannot use this brush because its already worn out. Needed a new one
+}
+
+public void destroyLibag () {
+    brush.replace();  // brand new brush that will destroy libag
+    libag.kuskos(); // libag slowly falling off
+    water.pour();   // pour more water to washed off libag
+    libag.soak();   // libag slowly falls off as water and brush combine their behaviors
+}
+
+public void reachConclusion () {
+    water.pour(); // pour it in the container
+    water.drink(); // drink it for hydration after battling with libag
+}
+}
---
+++ composition-lab/demo-class/CompositionDemoSilva.java
@@ -0,0 +1,56 @@
+// Main method created by Vinz Joseph Silva
+public class Main {
+    public static void main(String[] args) {
+        // Instantiated teammate classes 
+        Water waterDestroyer = new Water();
+        Brush brushWiper = new Brush();
+        Libag libagInvader = new Libag();
+
+        // teammate classes passed to the composed class
+        KilltheLibag operation = new KilltheLibag (waterDestroyer, brushWiper, libagInvader);
+
+        // Call all the methods used in KilltheLibag behavior
+        System.out.println("===== INTRODUCTION =====");
+            operation.introduceParts();
+        System.out.println("===== RISING ACTION =====");
+            operation.startCleaning();
+        System.out.println("===== CLIMAX =====");
+            operation.destroyLibag();
+        System.out.println("===== FALLING ACTION AND CONCLUSION");
+            operation.reachConclusion();
+}
+}
+
+/*
+
+- What are the **HAS-A relationships**?  
+The HAS-A relationships are those that have a connection with each other. It is basically 
+"something containing something." In my work, the execution of the class KilltheLibag HAS-
+A" water, brush, and libag.
+
+- Which classes were reused?  
+The classes reused are the water, brush, and libag. They are from my teammates and teacher
+classes and I used them for my composed class.
+
+- How does composition reduce coupling?
+Composition is like an "easiest arrangement of relationship" wherein the connection of the 
+classes are not directly tight. You can still modify something without restarting the rest
+of your code.
+
+- How did you implement high cohesion?  
+In my work, high cohesion is implemented by focusing in one goal. The class KilltheLibag
+is a class built to just show the building connection of water, brush, and libag (water and
+brush will help each other to beat libag. That is the only focus or target of implementing
+this code.)
+
+- How is cohesion maintained?  
+By staying in one goal for the code and not adding any useless or many types of function 
+in it.
+
+- Why is inheritance **NOT appropriate** here? 
+If I used inheritance in this code, I will be suffering to modify something from the code.
+Specifically on the multiplier task, wherein I need to update or change something from 
+my teammates class. Inheritance will make me perform a massive changes if I modify
+something from this code.
+
+*/
