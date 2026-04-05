362cbd91fb7936d5d7b2b9468f81d6a5dbeaf199
grimrex-dev <charlesjustinjavier@gmail.com>
2026-04-05 20:52:21 +0800

    Create RizzChildClass.java

---
+++ abstraction_and_polymorphism_lab/subclass/RizzChildClass.java
@@ -0,0 +1,39 @@
+/*
+*Created by Javier, Charles Justin D.
+*This class provides the child class of Rizz
+*/
+
+public abstract class Aura extends Rizz implements Sigma {
+     // Constructor
+    public Aura(String name, int auraLevel) {
+        super(name, auraLevel);
+    }
+}
+
+    //  Method Overriding 
+    @Override
+    public asbtract void showRizz() {
+        System.out.println(getName() + " shows unmatched sigma rizz 😎");
+    }
+
+    //  Method Overriding 
+    @Override
+    public void greet() {
+        System.out.println(getName() + " says: Hey there 😉");
+    }
+
+    @Override
+    public void compliment(String msg) {
+        System.out.println(getName() + " says: " + msg);
+    }
+
+    //  Method Overloading 
+    public void compliment() {
+        System.out.println(getName() + " says: You're amazing ✨");
+    }
+
+    public void compliment(String msg, int repeat) {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println(getName() + " says: " + msg);
+        }
+    }
