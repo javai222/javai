ba78f49e0d3b1f41ff061cf0497bf3ca94c8de20
Ryan James M. Balanon <balanonrm@students.nu-laguna.edu.ph>
2026-04-05 14:43:33 +0000

    created abstractaction and interface class

---
+++ abstraction_and_polymorphism_lab/interfacePkg/ConnectableLaptop.java
@@ -0,0 +1,17 @@
+// Interface representing a connectable laptop, and created by Balanon, Ryan James M.
+package abstraction;
+
+public interface ConnectableLaptop {
+
+    // Abstract method (must be implemented by child classes)
+    void connect();
+
+    // Getter and Setter for network name (encapsulation)
+    String getNetworkName();
+    void setNetworkName(String networkName);
+
+    // Default method
+    default void showConnectionStatus() {
+        System.out.println("Checking network connection...");
+    }
+}
