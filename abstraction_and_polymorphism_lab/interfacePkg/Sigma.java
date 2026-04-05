77333da4446ed61e40dcc464b094cb6a947aa852
grimrex-dev <charlesjustinjavier@gmail.com>
2026-04-05 12:06:01 +0000

    added child, main,and interface class

---
+++ abstraction_and_polymorphism_lab/interfacePkg/Sigma.java
@@ -0,0 +1,8 @@
+// interface created by Javier, Charles Justin
+
+package abstraction;
+
+public interface Sigma {
+    void greet();              
+    void compliment(String msg);
+}
---
+++ abstraction_and_polymorphism_lab/subclass/RizzChildClass.java
@@ -0,0 +1,41 @@
+/*
+*Created by Javier, Charles Justin D.
+*This class provides the child class of Rizz
+*/
+
+package abstraction;
+
+public abstract Aura extends Rizz implements Sigma {
+     // Constructor
+    public SigmaRizz(String name, int auraLevel) {
+        super(name, auraLevel);
+    }
+
+    // ✅ Method Overriding 
+    @Override
+    public void showRizz() {
+        System.out.println(getName() + " shows unmatched sigma rizz 😎");
+    }
+
+    // ✅ Method Overriding 
+    @Override
+    public void greet() {
+        System.out.println(getName() + " says: Hey there 😉");
+    }
+
+    @Override
+    public void compliment(String msg) {
+        System.out.println(getName() + " says: " + msg);
+    }
+
+    // ✅ Method Overloading 
+    public void compliment() {
+        System.out.println(getName() + " says: You're amazing ✨");
+    }
+
+    public void compliment(String msg, int repeat) {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println(getName() + " says: " + msg);
+        }
+    }
+}
