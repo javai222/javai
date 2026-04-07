298225d75d60ce917c6e68451764190c8ec48d48
Jasminnd <casapaojm@students.nu-laguna.edu.ph>
2026-04-07 02:36:08 +0000

    Added Abstraction Class and Interface

---
+++ abstraction_and_polymorphism_lab/interfacePkg/Wearable.java
@@ -0,0 +1,17 @@
+// Interface created by Casapao, Jasmin Ann M.
+
+package interfacePkg;
+
+interface Wearable {
+
+    //Abstract method
+    public void wear();
+
+    //default method
+    default void showUsage() {
+        Sytem.out.println("This item can be worn for fashion or protection.")
+    }
+}
+   
+    
+
