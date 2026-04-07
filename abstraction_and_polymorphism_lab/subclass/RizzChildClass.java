cc428d7cc72076a79816ee07b3195ed0a885c125
grimrex-dev <charlesjustinjavier@gmail.com>
2026-04-07 01:23:41 +0000

    diover fix

--- abstraction_and_polymorphism_lab/subclass/RizzChildClass.java
+++ abstraction_and_polymorphism_lab/subclass/RizzChildClass.java
@@ -3,37 +3,41 @@
 *This class provides the child class of Rizz
 */
 
-public abstract class Aura extends Rizz implements Sigma {
+package subclass;
+
+import abstraction.Rizz;
+import interfacePkg.Sigma;
+
+public class RizzChildClass extends Rizz implements Sigma {
      // Constructor
-    public Aura(String name, int auraLevel) {
+    public RizzChildClass(String name, int auraLevel) {
         super(name, auraLevel);
     }
-}
-
     //  Method Overriding 
     @Override
-    public asbtract void showRizz() {
+    public void showRizz() {
         System.out.println(getName() + " shows unmatched sigma rizz 😎");
     }
 
     //  Method Overriding 
     @Override
-    public abstract void greet() {
+    public void greet() {
         System.out.println(getName() + " says: Hey there 😉");
     }
 
     @Override
-    public abstract void compliment(String msg) {
+    public void compliment(String msg) {
         System.out.println(getName() + " says: " + msg);
     }
 
     //  Method Overloading 
-    public abstract void compliment() {
+    public void compliment() {
         System.out.println(getName() + " says: You're amazing ✨");
     }
 
-    public abstract void compliment(String msg, int repeat) {
+    public void compliment(String msg, int repeat) {
         for (int i = 0; i < repeat; i++) {
             System.out.println(getName() + " says: " + msg);
         }
     }
+}
