a6c22a0c202896523598e3e29dfed2e0d0e5c007
Vinz Joseph Silva <vinzjosephsilva@gmail.com>
2026-04-08 13:00:49 +0000

    Added subclass and demo class

---
+++ abstraction_and_polymorphism_lab/demo/AbstractionDemoSilva.java
@@ -0,0 +1,48 @@
+/*
+*Created by Silva, Vinz Joseph R.
+*This class provides the main class for the Glass class
+*/
+
+package demo;
+import subclass.DrinkingGlass;
+
+public class AbstractionDemoSilva {
+    public static void main(String[] args) {
+
+        DrinkingGlass drink = new DrinkingGlass ("Drinkware", "Clear");
+
+        // Runtime polymorphism (Overriding)
+        drink.useGlass();
+        drink.cleanGlass();
+        drink.breakGlass();
+
+        // Compile time Polymorphism (Overloading)
+        drink.useGlass("Soda");
+        drink.useGlass(100);
+    }
+}
+
+
+/* What abstract class did you create?
+ * // I created the Rizz class
+ * 
+ * What interface did you create?
+ * // I created the Mogger interface
+ * 
+ * What methods did you override?
+ * // I overrode useGlass(), cleanGlass(), and breakGlass() 
+ * 
+ * What methods did you overload?
+ * // I overloaded the useGlass()
+ * 
+ * Where does dynamic binding occur in your code?
+ * // It occurs when I called the overriden methods and creating an object as a reference for DrinkingGlass in the main class 
+  Which part shows polymorphism?
+ * // Polymorphism is executed when I override diffrent methods (Runtime polymorphism) and overload method (compile time polymorphism)
+  How does your design achieve low coupling?
+ * // The flexibility provided by polymorphism makes different classes or even methods to not be too muv=ch dependant to each other.
+      Also, the uses of default class makes it easy for the user to modify something in the interface without damaging the whole code.
+  How does your design achieve high cohesion?
+ * // High cohesion is achieved in this by applying the principles of abstraction by making it more efficient and focus on the most
+      functional part of the method without showing the long process under it.
+ */
---
+++ abstraction_and_polymorphism_lab/subclass/DrinkingGlass.java
@@ -0,0 +1,39 @@
+/*
+*Created by Silva, Vinz Joseph R.
+*This class provides the subclass of Glass
+*/
+
+package subclass;
+
+import abstraction.Glass;
+import interfacePkg.Breakable;
+
+public class DrinkingGlass extends Glass implements Breakable {
+    public DrinkingGlass(String glassType, String glassTransparency) {
+        super(glassType, glassTransparency);
+    }
+
+    // method overriding 
+    @Override
+    public void useGlass(){
+        System.out.println("Drinking water from the glass");
+    }
+
+    @Override
+    public void cleanGlass(){
+        System.out.println("Clean this glass after using it.");
+    }
+    @Override
+    public void breakGlass (){
+        System.out.println("Slips in your hands while cleaning it and falls to the ground");
+    }
+
+    public void useGlass(String softdrinks) {
+        System.out.println("Use this glass to drink " + softdrinks);
+    }
+
+    public void useGlass (int amount){
+        System.out.println("This softdrinks are priced as " + amount);
+    }
+
+}
