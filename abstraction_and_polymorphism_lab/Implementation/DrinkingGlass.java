85a285a743e2edf9b7730d90651fc0f67a840814
Jasminnd <casapaojm@students.nu-laguna.edu.ph>
2026-04-10 11:34:44 +0000

    Implentation and demo class

---
+++ abstraction_and_polymorphism_lab/Implementation/DrinkingGlass.java
@@ -0,0 +1,49 @@
+/*
+*Subclass created by Casapao, Abstraction and Interface Class created by Tismo
+*Created with the help of AI
+*/
+
+package implementation;
+
+import abstraction.Glass;
+import interfacePkg.Breakable;
+
+
+public class DrinkwareGlass extends Glass implements Breakable {
+
+    // Constructor
+    public DrinkwareGlass(String type, String transparency) {
+        super(type, transparency);
+    }
+
+    // OVERRIDING abstract methods
+    @Override
+    public void useGlass() {
+        System.out.println("This glass is used for drinking beverages.");
+    }
+
+    @Override
+    public void cleanGlass() {
+        System.out.println("Wash the drinkware glass with mild soap.");
+    }
+
+    @Override
+    public String getGlassPurpose() {
+        return "Drinking";
+    }
+
+    // OVERRIDING interface method
+    @Override
+    public void breakGlass() {
+        System.out.println("The drinkware glass shattered into pieces!");
+    }
+
+    // METHOD OVERLOADING
+    public void fillGlass() {
+        System.out.println("Filling the glass with water.");
+    }
+
+    public void fillGlass(String drink) {
+        System.out.println("Filling the glass with " + drink);
+    }
+}
---
+++ abstraction_and_polymorphism_lab/demo/AbstractionDemoCasapao.java
@@ -0,0 +1,76 @@
+/*
+*Subclass created by Casapao, Abstraction and Interface Class created by Tismo
+*/
+
+package demo;
+
+import abstraction.Glass;
+import subclass.DrinkwareGlass;
+import subclass.WindowGlass;
+
+
+public class AbstractionDemoCasapao {
+
+    public static void main(String[] args) {
+
+        /*
+         * DYNAMIC BINDING + POLYMORPHISM
+         * Using abstract reference
+         */
+        Glass glass1 = new DrinkwareGlass("Drinkware", "Clear");
+        Glass glass2 = new WindowGlass("Household", "Tinted");
+
+        // Dynamic binding occurs here
+        glass1.displayGlassInfo();
+        glass1.useGlass();
+        glass1.cleanGlass();
+
+        glass2.displayGlassInfo();
+        glass2.useGlass();
+        glass2.cleanGlass();
+
+        /*
+         * METHOD OVERLOADING demonstration
+         */
+        DrinkwareGlass drink = new DrinkwareGlass("Drinkware", "Clear");
+        drink.fillGlass();
+        drink.fillGlass("Juice");
+
+        WindowGlass window = new WindowGlass("Household", "Tinted");
+        window.openWindow();
+        window.openWindow(50);
+
+        /*
+         * INTERFACE METHOD
+         */
+        drink.breakGlass();
+        window.breakGlass();
+    }
+}
+
+/*
+
+1. What abstract class did you create?
+the abstract class that I created is from Tismo which is the Glass class.
+
+2. What interface did you create?
+The interface that I created is the breakable interface.
+
+3. What methods did you override?
+The methods that I override is useGlass(), cleanGlass(), getGlassPurpose() and breakGlass().
+
+4. What methods did you overload?
+The methods that i overloaded are fillGlass() and fillGlass(String drink), in DrinkwareGlass openWindow() and openWindow(int width) in WindowGlass
+
+5. Where does dynamic binding occur?
+When using Glass glass1 = new DrinkwareGlass(...); Glass glass2 = new WindowGlass(...); The method calls are resolved at runtime.
+
+6. Which part shows polymorphism?
+- Using Glass reference for different objects: Glass glass1, glass2
+
+7. How does your design achieve low coupling?
+- Classes depend on abstraction (Glass and Breakable), not concrete classes.
+
+8. How does your design achieve high cohesion?
+- Each class has a focused responsibility for example drinking glass is for drinking and window glass is for window.
+*/
