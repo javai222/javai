686eebf4c6fd06be509edac28bfc6fd4fbea1638
Lord Ulrick A. De Leon <deleonlua@students.nu-laguna.edu.ph>
2026-04-15 10:17:52 +0000

    Created Abstract Class and Interface

---
+++ abstraction_and_polymorphism_lab/abstraction/Lego.java
@@ -0,0 +1,66 @@
+// This Abstract Class was created by De Leon, Lord Ulrick A.
+
+package abstraction;
+
+public abstract class Lego {
+
+    // Attributes
+    private String legoSetName;     // Name of the lego set
+    private int legoPiecesQuantity  // Number of lego pieces in the set
+    private double legoSetPrice;    // Price of the lego set
+
+    // Constructor
+    public Lego(String legoSetName, int legoPiecesQuantity, double legoSetPrice) {
+        this.legoSetName = legoSetName;
+        this.legoPiecesQuantity = legoPiecesQuantity
+        this.legoSetPrice = legoSetPrice;
+    }
+
+    // Getters and Setters
+    public String getlegoSetName() {
+        return legoSetName;
+    }
+
+    public String setlegoSetName(String legoSetName) {
+        if(legoSetName != null) {
+            this.legoSetName = legoSetName;
+        } else {
+            System.out.println("This is an invalid Name");
+        }
+    }
+
+    public int getlegoPiecesQuantity() {
+        return legoPiecesQuantity;
+    }
+
+    public int setlegoPiecesQuantity(int legoPiecesQuantity) {
+        if(legoPiecesQuantity > 0) {
+            this.legoPiecesQuantity = legoPiecesQuantity
+        } else {
+            System.out.println("Number of pieces should not be 0 or below");
+        }
+    }
+
+    public double getlegoSetPrice() {
+        return legoSetPrice;
+    }
+
+    public double setlegoSetPrice(double legoSetPrice) {
+        if(legoSetPrice >= 0.0) {
+            this.legoSetPrice = legoSetPrice;
+        } else {
+            System.out.println("Price cannot be a negative integer!");
+        }
+    }
+
+    // Abstract Method that subclass must implement
+    public abstract void buildLegoSet();
+
+    // Concrete Method
+    public void displayLegoSetDetails() {
+        System.out.println("\nDetails of the Lego Set:");
+        System.out.println("Name: " + legoSetName);
+        System.out.println("Number of pieces: " + legoPiecesQuantity);
+        System.out.println("Price:" + legoSetPrice);
+    }
+}
---
+++ abstraction_and_polymorphism_lab/interfacePkg/LegoActions.java
@@ -0,0 +1,14 @@
+// This Interface was created by De Leon, Lord Ulrick A.
+
+package interfacePkg;
+
+public interface LegoActions {
+
+    // Abstract method
+    void playWithLego();
+
+    // Default Method
+    default void cleanUpLego() {
+        System.out.println("Cleaning up the lego pieces after playing.")
+    }
+}
