39da692db759d1d6a026c03bfecaf7295b018f8a
Jasminnd <casapaojm@students.nu-laguna.edu.ph>
2026-04-17 20:19:50 +0800

    Add DivisionByZeroException class for error handling

---
+++ exceptions-and-errors-lab/JHY/exceptions/DivisonByZeroException.java
@@ -0,0 +1,9 @@
+// created by Casapao, Dolormente, and Tismo
+package exceptions;
+
+// Unchecked exception
+public class DivisionByZeroException {
+  public DivisionByZeroException(String message) {
+    super(message);
+  }
+}
