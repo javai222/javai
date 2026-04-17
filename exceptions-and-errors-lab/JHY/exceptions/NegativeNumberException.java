1ee661656c54bbf85cab1c3141590dc3a75d3cff
Jasminnd <casapaojm@students.nu-laguna.edu.ph>
2026-04-17 20:46:28 +0800

    Update NegativeNumberException.java

--- exceptions-and-errors-lab/JHY/exceptions/NegativeNumberException.java
+++ exceptions-and-errors-lab/JHY/exceptions/NegativeNumberException.java
@@ -2,7 +2,7 @@
 package exceptions;
 
 // Unchecked Exception
-public class NegativeNumberException {
+public class NegativeNumberException extends RuntimeExeption {
   public NegativeNumberException(String message) {
     super(message)
   }
