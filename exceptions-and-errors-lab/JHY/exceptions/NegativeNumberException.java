75fd3d8a6610f8bed12dc0a51df4438041acb87f
Jasminnd <casapaojm@students.nu-laguna.edu.ph>
2026-04-17 20:28:40 +0800

    Create NegativeNumberException.java

---
+++ exceptions-and-errors-lab/JHY/exceptions/NegativeNumberException.java
@@ -0,0 +1,9 @@
+// Created by Casapao, Dolormente, and Tismo
+package exceptions;
+
+// Unchecked Exception
+public class NegativeNumberException {
+  public NegativeNumberException(String message) {
+    super(message)
+  }
+}
