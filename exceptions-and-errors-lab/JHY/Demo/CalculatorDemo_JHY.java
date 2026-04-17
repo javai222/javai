50ccf005edeec91c8e2424533e4b9f48225dc467
Baguette <tismoye@students.nu-laguna.edu.ph>
2026-04-17 21:03:51 +0800

    Update CalculatorDemo_JHY.java

--- exceptions-and-errors-lab/JHY/Demo/CalculatorDemo_JHY.java
+++ exceptions-and-errors-lab/JHY/Demo/CalculatorDemo_JHY.java
@@ -3,10 +3,25 @@ package Demo;
 import exceptions.DivisionByZeroException;
 import exceptions.InvalidInputException;
 import exceptions.NegativeNumberException;
-import models.Calculator;
+import model.Calculator;
 
 public class CalculatorDemo_JHY {
     public static void main (String [] args){
-      
-      }
+        Calculator cal = new Calculator();
+
+        System.out.println("=== CALCULATOR DEMO ===\n");
+        try {
+            double num1 = cal.getValidNumber ("16");
+            double num2 = cal.getValidNumber ("67");
+
+            double result = cal.add(num1,num2);
+            System.out.println("Addition Result: " + result);
+        } catch (InvalidInputException invinp) {
+            System.out.println("Checked Exception: " + invinp.getMessage());
+        } finally {
+            System.out.println("Addition operation attempted.");
+        }
+
+        System.out.println();
+    }
 }
