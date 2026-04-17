360fe575d546782ebb0cc8787becb12e665fc01a
Lord Ulrick A. De Leon <deleonlua@students.nu-laguna.edu.ph>
2026-04-17 21:54:23 +0800

    Implement StoreReceipt class with receipt methods

--- abstraction_and_polymorphism_lab/Implementation/StoreReceipt.java
+++ abstraction_and_polymorphism_lab/Implementation/StoreReceipt.java
@@ -1,1 +1,41 @@
-fix
+// This subclass was created by De Leon, Lord Ulrick A.
+
+package Implementation1;
+
+import abstraction.Receipt;
+import interfacePkg.Printable;
+
+public class StoreReceipt extends Receipt implements Printable {
+
+    public StoreReceipt(String customerName, int receiptNumber) {
+        super(customerName, receiptNumber);
+    }
+
+    // Overriding (Abstract Method)
+    @Override
+    public void generateReceipt() {
+        System.out.println("\n[Store Receipt]");
+        displayReceiptInfo();
+    }
+
+    // Overriding (Abstract Method)
+    @Override
+    public String getReceiptDetails() {
+        return "Store purchase processed.";
+    }
+
+    // Overriding (Interface Method)
+    @Override
+    public void printReceipt() {
+        System.out.println("Printing Store Receipt...");
+        generateReceipt();
+        confirmPrint();
+    }
+
+    // Method Overloading
+    public void generateReceipt(String note) {
+        System.out.println("\n[Store Receipt with Note]");
+        displayReceiptInfo();
+        System.out.println("Note: " + note);
+    }
+}
