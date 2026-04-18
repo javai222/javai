54b21372d707f90682a2f287e0a017b364ca3963
Vinz Joseph Silva <vinzjosephsilva@gmail.com>
2026-04-18 04:26:04 +0000

    Created Calculator Files

--- exceptions-and-errors-lab/Silva_Plantilla_DeLeon/Exceptions/NegativeNumbersException.java
+++ exceptions-and-errors-lab/Silva_Plantilla_DeLeon/Exceptions/NegativeNumbersException.java
@@ -2,8 +2,8 @@ package Exceptions;
 
 /* This is exception is unchecked, wherein the system will throw an exception
 if the user input negative numbers with the operators*/ 
-public class NegativeNumberException extends RuntimeException {
-    public NegativeNumberException (String message){
+public class NegativeNumbersException extends RuntimeException {
+    public NegativeNumbersException (String message){
         super (message);
     }
 }
