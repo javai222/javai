784e553accaf61a4b55e713a3afad68defae8fc2
Kenneth Saan <kennethsaan16@gmail.com>
2026-04-18 10:20:35 +0000

    logic

--- exceptions-and-errors-lab/Saan_Javier_Mercado/Logic/UnaryOp.java
+++ exceptions-and-errors-lab/Saan_Javier_Mercado/Logic/UnaryOp.java
@@ -3,7 +3,49 @@ import Contract.unaryOp;
 
 public class UnaryOp extends unaryOp {
     
+    @Override
+    public double squareRoot(double a) {
+        if (a < 0)
+            throw new IllegalArgumentException("Cannot take square root of a negative number");
+        return Math.sqrt(a);
+    }
 
+    @Override
+    public double factorial(double a) {
+        if (a < 0)
+            throw new IllegalArgumentException("Cannot take factorial of a negative number");
+        if (a != (int) a)
+            throw new IllegalArgumentException("Cannot take factorial of a non-integer");
+        double result = 1;
+        for (int i = 1; i <= (int) a; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    @Override
+    public double logarithm(double a) {
+        if (a <= 0)
+            throw new IllegalArgumentException("Cannot take logarithm of non-positive numbers");
+        return Math.log(a);
+    }
+
+    @Override
+    public double sine(double a) {
+        return Math.sin(a);
+    }
+
+    @Override
+    public double cosine(double a) {
+        return Math.cos(a);
+    }
+
+    @Override
+    public double tangent(double a) {
+        if (Math.cos(a) == 0)
+            throw new IllegalArgumentException("Tangent is undefined at this angle");
+        return Math.tan(a);
+    }
 
 
 
