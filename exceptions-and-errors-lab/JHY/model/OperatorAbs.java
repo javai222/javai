ce61806c79857aa3f12a1d6cda0778170c6d33fe
Henry <Henrydolormente11706@gmail.com>
2026-04-18 20:51:33 +0800

    Change abstract methods to protected in OperatorAbs

--- exceptions-and-errors-lab/JHY/model/OperatorAbs.java
+++ exceptions-and-errors-lab/JHY/model/OperatorAbs.java
@@ -3,17 +3,17 @@ package calculator;
 
 public abstract class OperatorAbs {
     // abstract method for addition
-    public abstract int add(int a, int b);
-    public abstract double add(double a, double b);
+    protected abstract int add(int a, int b);
+    protected abstract double add(double a, double b);
 
     // abstract method for subtraction
-    public abstract int subtract(int a, int b);
-    public abstract double subtract(double a, double b);
+    protected abstract int subtract(int a, int b);
+    protected abstract double subtract(double a, double b);
 
     // abstract method for multiplication
-    public abstract int multiply(int a, int b);
-    public abstract double multiply(double a, double b);
+    protected abstract int multiply(int a, int b);
+    protected abstract double multiply(double a, double b);
 
     // abstract method for division and to handle the invalid input
-    public abstract double divide(double a, double b) throws InvalidInputException;
+    protected abstract double divide(double a, double b) throws InvalidInputException;
 }
