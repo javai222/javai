00066552f7aed72be9042e1421df2d720db04cd5
Vinz Joseph Silva <vinzjosephsilva@gmail.com>
2026-04-18 04:06:59 +0000

    Added calculator files

---
+++ exceptions-and-errors-lab/Silva_Plantilla_DeLeon/Exceptions/DivisionByZeroException.java
@@ -0,0 +1,9 @@
+package Exception;
+
+/* This exception is unchecked, wherein it shows that the system will throw an
+exception if the user divide anumber by zero*/
+public class DivisionByZeroException extends RuntimeException {
+    public DivisionByZeroException (Sting message){
+        super(message);
+    }
+}
