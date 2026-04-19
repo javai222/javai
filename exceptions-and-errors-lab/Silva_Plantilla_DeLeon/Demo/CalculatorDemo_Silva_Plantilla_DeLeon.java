1f04312e1b37e2ee0dac6b29f7c4d8ed454692a4
Vinz Joseph Silva <vinzjosephsilva@gmail.com>
2026-04-19 07:48:49 +0000

    Added Comments

--- exceptions-and-errors-lab/Silva_Plantilla_DeLeon/Demo/CalculatorDemo_Silva_Plantilla_DeLeon.java
+++ exceptions-and-errors-lab/Silva_Plantilla_DeLeon/Demo/CalculatorDemo_Silva_Plantilla_DeLeon.java
@@ -1,5 +1,22 @@
 // Demo Class created by De Leon, Plantilla, and Silva
 
+/* *What exceptions did you create?
+-We created DivisionByZeroException, InvalidInputException, and NegativeNumberException.
+*Which are checked vs unchecked?
+- The InvalidInputException is a checked exception, while DivisionByZeroException and NegativeNumberException are unchecked exceptions.
+*Where are exceptions thrown?
+- The exceptions are thrown inside the Calculator class methods whenever invalid inputs like zero division or negative numbers are detected.
+*Where are they handled?
+- They are handled in the CalculatorDemo_Silva_Plantilla_DeLeon class using separate try-catch blocks for each scenario.
+*Where does propagation occur?
+- When exception is thrown inside the calculator methods  (like checkValidity(), checkNegative(), or divide()) but not handled there.
+Instead, the process happens in the main demo.
+*How did you apply OOP concepts?
+Encapsulation - the datas that is put on a specific modifier (the protected modifier on OperatorAbs)
+Inheritance - the extension of exceptions in our created exceptions (checked and unchecked exceptions)
+Abstraction - The operator class wherein the process of it was not shown and only showcased in the calculator file.
+Polymorphism - The method overriding and overloading of the different operators.
+*/
 package Demo;
 
 import java.util.Scanner;
