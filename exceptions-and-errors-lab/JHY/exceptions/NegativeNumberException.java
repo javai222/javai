964cc93c8117a8ddf43363c30af0f2b5d3637aaf
Henry <Henrydolormente11706@gmail.com>
2026-04-19 22:26:35 +0800

    Update and rename NegativeNumberExceptions.java to NegativeNumberException.java

---
+++ exceptions-and-errors-lab/JHY/exceptions/NegativeNumberException.java
@@ -0,0 +1,9 @@
+// Created by Casapao, Dolormente, and Tismo
+package exceptions;
+
+// Unchecked Exception
+public class NegativeNumberException extends RuntimeException {
+  public NegativeNumberException(String message) {
+    super(message)
+  }
+}
--- exceptions-and-errors-lab/JHY/exceptions/NegativeNumberExceptions.java
+++
@@ -1,9 +0,0 @@
-// Created by Casapao, Dolormente, and Tismo
-package exceptions;
-
-// Unchecked Exception
-public class NegativeNumberExceptions extends exceptions {
-  public NegativeNumberExceptions(String message) {
-    super(message);
-  }
-}
