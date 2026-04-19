54aae0c76d4b360222726c1d872d85afdb0b9472
Vinz Joseph Silva <vinzjosephsilva@gmail.com>
2026-04-19 12:56:17 +0800

    Completed Calculator Files (#635)

--- exceptions-and-errors-lab/Silva_Plantilla_DeLeon/Models/Calculator.java
+++ exceptions-and-errors-lab/Silva_Plantilla_DeLeon/Models/Calculator.java
@@ -1,49 +1,81 @@
 package Models;
 
 import Exceptions.DivisionByZeroException;
-import Exceptions.InvalidInputValuesException;
+import Exceptions.InvalidInputException;
 import Exceptions.NegativeNumbersException;
 
 public class Calculator extends OperatorsAbs {
 
+    public double checkValidity(String input) throws InvalidInputException {
+    try {
+        return Double.parseDouble(input);
+    } catch (NumberFormatException val) {
+        throw new InvalidInputException("Invalid input! Only numbers are allowed.");
+    }
+  }
+
     // simple validation
-    private void checkNegative(int a, int b) {
+    private void checkNegative(double a, double b) {
         if (a < 0 || b < 0) {
             throw new NegativeNumbersException("Negative numbers are not allowed.");
         }
     }
 
     @Override
-    public double add(int a, int b) {
-        checkNegative(a, b);
-        return a + b;
-    }
-
-    // METHOD OVERLOADING
     public double add(double a, double b) {
+        checkNegative(a,b);
         return a + b;
     }
 
     @Override
-    public double subtract(int a, int b) {
+    public int add(int a, int b) {
+        checkNegative(a,b);
+        return a + b;
+    }
+    
+    @Override
+    public double subtract(double a, double b) {
         checkNegative(a, b);
         return a - b;
     }
 
     @Override
-    public double multiply(int a, int b) {
+    public int subtract(int a, int b) {
+        checkNegative(a,b);
+        return a - b;
+    }
+
+    @Override
+    public double multiply(double a, double b) {
         checkNegative(a, b);
         return a * b;
     }
 
     @Override
-    public double divide(int a, int b) {
+    public int multiply(int a, int b) {
+        checkNegative(a, b);
+        return a * b;
+    }
+
+    @Override
+    public double divide(double a, double b) {
         checkNegative(a, b);
 
         if (b == 0) {
             throw new DivisionByZeroException("Cannot divide by zero.");
         }
 
-        return (double) a / b;
+        return a / b;
+    }
+
+    @Override
+    public int divide(int a, int b) {
+        checkNegative(a, b);
+
+        if (b == 0) {
+            throw new DivisionByZeroException("Cannot divide by zero.");
+        }
+
+        return a / b;
     }
 }
