21356880365d8111bebcc6e0b3bc1263a4de6a49
Henry <Henrydolormente11706@gmail.com>
2026-04-19 22:26:54 +0800

    Update and rename CalculatorJHY.java to Calculator.java

--- exceptions-and-errors-lab/JHY/model/Calculator.java
+++ exceptions-and-errors-lab/JHY/model/Calculator.java
@@ -6,12 +6,12 @@
 
 package models;
 
-import exceptions.DivisionByZeroExceptions;
-import exceptions.InvalidInputExceptions;
-import exceptions.NegativeNumberExceptions;
+import exceptions.DivisionByZeroException;
+import exceptions.InvalidInputException;
+import exceptions.NegativeNumberException;
 
 // Calculator class inherits abstract methods from OperatorAbs
-public class CalculatorJHY extends OperatorAbs {
+public class Calculator extends OperatorAbs {
 
   // For Addition
   @Override
@@ -45,22 +45,22 @@ public class CalculatorJHY extends OperatorAbs {
   }  
   // For divison by Zero
   @Override
-  protected double divide(double a, double b) throws DivisionByZeroExceptions {
+  protected double divide(double a, double b) throws DivisionByZeroException {
     if (b == 0)
-        throw new DivisionByZeroExceptions("Division by 0 is not possible.");
+        throw new DivisionByZeroException("Division by 0 is not possible.");
     return (double) a / b;
   }
   // Checking if the value is a negative number
   public void checkNegative(double value) {
     if (value < 0) 
-        throw new NegativeNumberExceptions("Negative values are not allowed.");
+        throw new NegativeNumberException("Negative values are not allowed.");
   }
   // Converts string input to double
-  public double getValidNumber(String input) throws InvalidInputExceptions {
+  public double getValidNumber(String input) throws InvalidInputException {
     try {
         return Double.parseDouble(input);
     } catch (NumberFormatException inv) {
-        throw new InvalidInputExceptions("Invalid input! Only numbers are allowed.");
+        throw new InvalidInputException("Invalid input! Only numbers are allowed.");
     }
   }
 
