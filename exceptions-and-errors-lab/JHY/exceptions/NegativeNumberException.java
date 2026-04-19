27ce07d703b339990bce475ef32dee3e7c23734a
Henry <Henrydolormente11706@gmail.com>
2026-04-19 21:49:03 +0800

    Update NegativeNumberException.java

--- exceptions-and-errors-lab/JHY/exceptions/NegativeNumberException.java
+++ exceptions-and-errors-lab/JHY/exceptions/NegativeNumberException.java
@@ -4,6 +4,6 @@ package exceptions;
 // Unchecked Exception
 public class NegativeNumberException extends RuntimeException {
   public NegativeNumberException(String message) {
-    super(message)
+    super(message);
   }
 }
