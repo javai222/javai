9c6dc802fda12f0ae590e048c7394bded24bc2ac
Henry <Henrydolormente11706@gmail.com>
2026-04-19 22:32:30 +0800

    Update CalculatorDemo_JHY.java

--- exceptions-and-errors-lab/JHY/Demo/CalculatorDemo_JHY.java
+++ exceptions-and-errors-lab/JHY/Demo/CalculatorDemo_JHY.java
@@ -34,7 +34,7 @@ package Demo;
 import exceptions.DivisionByZeroException;
 import exceptions.InvalidInputException;
 import exceptions.NegativeNumberException;
-import model.Calculator;
+import models.Calculator;
 
 public class CalculatorDemo_JHY {
     public static void main (String [] args){
