fe2d98229bf95ccb937e341f9ddd00281e66152f
pogita <139799245+Astolfo-Senpai@users.noreply.github.com>
2026-04-20 13:55:09 +0000

    Added Engine, Robot, Demo in composition

---
+++ composition_lab/Composed/Robot.java
@@ -0,0 +1,37 @@
+package composition_lab;
+
+public class Robot {
+
+    private Brain brain;      // HAS-A Brain
+    private Engine engine;    // HAS-A Engine
+    private Battery battery;  // HAS-A Battery
+
+    public Robot(Brain brain , Engine engine, Battery battery) {
+        this.brain = brain;
+        this.engine = engine;
+        this.battery = battery;
+    }
+
+    public void startRobot() {
+        if (battery.getChargeLevel() > 0) {
+            engine.startEngine();
+            brain.think();
+            battery.drain(10);
+            System.out.println("Robot is now active.");
+        } else {
+            System.out.println("Battery is dead. Please recharge.");
+        }
+    }
+
+    public void panic() {
+        brain.panicMode();
+        battery.drain(20);
+        System.out.println("Robot is panicking!");
+    }
+
+    public void shutdownRobot() {
+        engine.stopEngine();
+        System.out.println("Robot is shutting down.");
+    }
+
+}
