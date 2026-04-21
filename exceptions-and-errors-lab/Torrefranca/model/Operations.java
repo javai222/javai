3f27f6108d0fba81210e96585fd32a0c15a9858d
v1nncy <torrefrancavp@students.nu-laguna.edu.ph>
2026-04-22 07:09:14 +0800

    Add Operations class with basic arithmetic methods

---
+++ exceptions-and-errors-lab/Torrefranca/model/Operations.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Operations {
+
+    // ADD
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    // SUBTRACT
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // MULTIPLY
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // DIVIDE
+    public double divide(double a, double b) {
+        return a / b;
+    }
+}
