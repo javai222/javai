7a245ccdfac3a14f58c67ff0b1406d9defeddacc
Ryan James M. Balanon <balanonrm@students.nu-laguna.edu.ph>
2026-04-21 14:14:42 +0000

    added folder

---
+++ exceptions-and-errors-lab/BCM/Models/Operations.java
@@ -0,0 +1,21 @@
+
+package models;
+
+public abstract class OperatorsAbs {
+
+    // ADDITION
+    public abstract int add(int a, int b);
+    public abstract double add(double a, double b);
+
+    // SUBTRACTION
+    public abstract int subtract(int a, int b);
+    public abstract double subtract(double a, double b);
+
+    // MULTIPLICATION
+    public abstract int multiply(int a, int b);
+    public abstract double multiply(double a, double b);
+
+    // DIVISION
+    public abstract int divide(int a, int b) throws Exception;
+    public abstract double divide(double a, double b) throws Exception;
+}
