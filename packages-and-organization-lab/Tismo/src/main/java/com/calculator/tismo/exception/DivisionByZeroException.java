91521a8ddb95e942e5a0c303f11237a6975501be
Baguette <tismoye@students.nu-laguna.edu.ph>
2026-04-26 14:14:31 +0000

    package files

---
+++ packages-and-organization-lab/Tismo/src/main/java/com/calculator/tismo/exception/DivisionByZeroException.java
@@ -0,0 +1,9 @@
+// created by Casapao, Dolormente, and Tismo
+package com.calculator.tismo.exception;
+
+// Unchecked exception
+public class DivisionByZeroException extends RuntimeException {
+  public DivisionByZeroException(String message) {
+    super(message);
+  }
+}
---
+++ packages-and-organization-lab/Tismo/src/main/java/com/calculator/tismo/exception/NegativeNumberException.java
@@ -0,0 +1,9 @@
+// Created by Casapao, Dolormente, and Tismo
+package com.calculator.tismo.exception;
+
+// Unchecked Exception
+public class NegativeNumberException extends RuntimeException {
+  public NegativeNumberException(String message) {
+    super(message);
+  }
+}
