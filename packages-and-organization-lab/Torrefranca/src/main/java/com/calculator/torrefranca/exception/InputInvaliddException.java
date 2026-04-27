c6e95f3652c0b8a60d8aa1aea2846cce70e138f9
v1nncy <torrefrancavp@students.nu-laguna.edu.ph>
2026-04-27 20:47:44 +0800

    Update InputInvaliddException.java

--- packages-and-organization-lab/Torrefranca/src/main/java/com/calculator/torrefranca/exception/InputInvaliddException.java
+++ packages-and-organization-lab/Torrefranca/src/main/java/com/calculator/torrefranca/exception/InputInvaliddException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package com.calculator.torrefranca.exception;
 
 public class InputInvaliddException extends RuntimeException {
     public InputInvaliddException(String message) {
