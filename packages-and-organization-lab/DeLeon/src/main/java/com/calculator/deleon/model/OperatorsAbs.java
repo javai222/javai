ce9bf38781fdd27831633c350a83de5f52e157d8
Lord Ulrick A. De Leon <deleonlua@students.nu-laguna.edu.ph>
2026-04-27 21:48:08 +0800

    Completed Package Lab Activity (#714)

---
+++ packages-and-organization-lab/DeLeon/src/main/java/com/calculator/deleon/model/OperatorsAbs.java
@@ -0,0 +1,22 @@
+package com.calculator.deleon.model;
+
+// abstract class for operators
+public abstract class OperatorsAbs {
+
+// abstract class for addition
+protected abstract int add(int a, int b);
+protected abstract double add (double a, double b);
+
+// abstract class for subtraction
+protected abstract int subtract(int a, int b);
+protected abstract double subtract (double a, double b);
+
+// abstract class for multiplication
+protected abstract int multiply(int a, int b);
+protected abstract double multiply (double a, double b);
+
+// abstract class for division
+protected abstract int divide(int a, int b);
+protected abstract double divide (double a, double b);
+
+}
