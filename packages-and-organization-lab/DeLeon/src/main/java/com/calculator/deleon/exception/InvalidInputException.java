28485cf4c42a37cb949704a4c7bc2489cb0de669
Lord Ulrick A. De Leon <deleonlua@students.nu-laguna.edu.ph>
2026-04-27 13:35:27 +0000

    Completed Package Lab Activity

---
+++ packages-and-organization-lab/DeLeon/src/main/java/com/calculator/deleon/exception/InvalidInputException.java
@@ -0,0 +1,15 @@
+package com.calculator.deleon.exception;
+
+/* This exception is checked, the system will throw an exception if the user
+input an invalid number to use for operations*/
+public class InvalidInputException extends Exception {
+
+    // Constructor without message
+    public InvalidInputException () {
+        super();
+    }
+    // Constructor with message
+    public InvalidInputException (String message){
+        super (message);
+    }
+}
