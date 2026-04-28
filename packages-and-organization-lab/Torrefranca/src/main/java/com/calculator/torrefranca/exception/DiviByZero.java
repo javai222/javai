6a0d9d506c2ed17dbf1cea20e42c2fc5cfd4ef4e
v1nncy <torrefrancavp@students.nu-laguna.edu.ph>
2026-04-28 18:23:14 +0800

    p (#710)
    * p
    
    * Change package declaration in Calculatorrrr.java
    
    * Update Operations.java
    
    * Update CalculatorDemo_Torrefranca.java
    
    * Update DiviByZero.java
    
    * Update InputInvaliddException.java
    
    * Update NegativeNum.java
    
    * Update CalculatorDemo_Torrefranca.java
    
    * Fix import statements in Calculatorrrr.java
    
    * Update CalculatorDemo_Torrefranca.java
    
    * Update CalculatorDemo_Torrefranca.java
    
    * Update Calculatorrrr.java
    
    * Update Operations.java
    
    * Update CalculatorDemo_Torrefranca.java
    
    * Update CalculatorDemo_Torrefranca.java
    
    * Update CalculatorDemo_Torrefranca.java

---
+++ packages-and-organization-lab/Torrefranca/src/main/java/com/calculator/torrefranca/exception/DiviByZero.java
@@ -0,0 +1,7 @@
+package com.calculator.torrefranca.exception;
+
+public class DiviByZero extends RuntimeException {
+    public DiviByZero(String message) {
+        super(message);
+    }
+}
---
+++ packages-and-organization-lab/Torrefranca/src/main/java/com/calculator/torrefranca/exception/NegativeNum.java
@@ -0,0 +1,7 @@
+package com.calculator.torrefranca.exception;
+
+public class NegativeNum extends RuntimeException {
+    public NegativeNum(String message) {
+        super(message);
+    }
+}
