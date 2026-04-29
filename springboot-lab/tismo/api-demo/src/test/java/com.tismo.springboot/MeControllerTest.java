1246274ba04fdd2e505ac7a4532425f05f00d4d7
Baguette <tismoye@students.nu-laguna.edu.ph>
2026-04-29 09:59:16 +0000

    pls

--- springboot-lab/tismo/api-demo/src/test/java/com.tismo.springboot/MeControllerTest.java
+++ springboot-lab/tismo/api-demo/src/test/java/com.tismo.springboot/MeControllerTest.java
@@ -1,6 +1,5 @@
 package com.tismo.springboot;
 
-import com.tismo.springboot.controller.MeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
