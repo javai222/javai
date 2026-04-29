8deb01ffa6d81b9260e2323d6e2ee1cce3c4f19d
Baguette <tismoye@students.nu-laguna.edu.ph>
2026-04-29 11:06:37 +0000

    Spring boot lab files

--- springboot-lab/tismo/api-demo/src/main/java/com/tismo/springboot/controller/MeController.java
+++ springboot-lab/tismo/api-demo/src/main/java/com/tismo/springboot/controller/MeController.java
@@ -1,3 +1,23 @@
+/* 
+Created by Yvette Tismo
+* How did you create your Spring Boot project?
+    - I created my project with the help of Spring Initializr, Intellij/codespace 
+
+* What is the purpose of the @RestController annotation?
+    - It tells spring that HTTP requests and that the return values of its methods should be automatically converted into JSON and sent in the HTTP response body.
+
+* What does the @GetMapping("/me") annotation do?
+    - @GetMapping("/me") maps HTTP GET requests sent to the /me endpoint to a specific method in the controller.
+
+* Why did you change from returning a String to a Map?
+    - So that it is more flexible, organized and prettier
+
+* How does Spring Boot handle JSON responses automatically?
+    - Spring Boot uses a library like Jackson to automatically convert Java objects
+
+*/
+
+
 package com.tismo.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
