72c7c2f80f83ff1acda2337588f06e008ff28788
grimrex-dev <charlesjustinjavier@gmail.com>
2026-04-29 17:32:34 +0800

    Fix package declaration in MeController

--- springboot-lab/Javier/api-demo/src/main/java/com/javier/springboot/controller/MeController.java
+++ springboot-lab/Javier/api-demo/src/main/java/com/javier/springboot/controller/MeController.java
@@ -1,4 +1,4 @@
-package com.javier.springboot.controller.MeController;
+package com.javier.springboot.controller.;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
