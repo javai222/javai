8a46a4738136cb87644d2ca890a553d35ed94df0
Jasminnd <casapaojm@students.nu-laguna.edu.ph>
2026-04-29 19:18:27 +0800

    Completed Spring Boot REST API (#736)

---
+++ springboot-lab/casapao/api-demo/src/test/java/com/casapao/springboot/ApiDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.casapao.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ApiDemoApplication.class)
+class ApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
