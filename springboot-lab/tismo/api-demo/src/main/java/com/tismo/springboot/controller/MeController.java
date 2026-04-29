533230d0857075935a6900aea0255352c9fd9853
Baguette <tismoye@students.nu-laguna.edu.ph>
2026-04-29 09:34:07 +0000

    spring boot lab

---
+++ springboot-lab/tismo/api-demo/src/main/java/com/tismo/springboot/controller/MeController.java
@@ -0,0 +1,20 @@
+package com.tismo.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class MeController {
+
+    @GetMapping("/me")
+    public Map<String, Object> getMe() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("name", "Yvette Germaine E. Tismo");
+        response.put("studentId", "2025-1027104");
+        response.put("course", "Java Programming");
+        response.put("message", "Learning Spring Boot REST APIs!");
+        return response;
+    }
+}
