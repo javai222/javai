2bde9e39a141ffc6e3afb64b842c8cf58d135d27
Baguette <tismoye@students.nu-laguna.edu.ph>
2026-05-01 00:00:36 +0800

    Add answers to questions for reflection (#750)

--- oop-databases-lab/Tismo/app-demo/src/main/java/com/tismo/app/AppDemoApplication.java
+++ oop-databases-lab/Tismo/app-demo/src/main/java/com/tismo/app/AppDemoApplication.java
@@ -1,3 +1,14 @@
+/*
+*What changed from previous lab?
+	- There are no entities, services, repositories and overall there are more dependencies
+*Why database is better?
+	- A database is better than storing data in memory or files because it is reliable and well structured
+*How JPA helped?
+	- It helped by acting as a bridge between objects and the database
+*What changed when switching DB?
+	- When the server reloads the previous saved data will still be saved
+*/
+
 package com.tismo.app;
 
 import org.springframework.boot.SpringApplication;
