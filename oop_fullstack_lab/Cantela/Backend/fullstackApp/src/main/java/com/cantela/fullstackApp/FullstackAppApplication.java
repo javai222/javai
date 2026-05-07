e37dd395cf13603982c5164cfaef9885d429c2e9
Chris Nelson Cantela <filipinogamer64@gmail.com>
2026-05-07 23:37:42 +0800

    Commit fullstack (#761)

---
+++ oop_fullstack_lab/Cantela/Backend/fullstackApp/src/main/java/com/cantela/fullstackApp/FullstackAppApplication.java
@@ -0,0 +1,13 @@
+package com.cantela.fullstackApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackAppApplication.class, args);
+	}
+
+}
---
+++ oop_fullstack_lab/Cantela/Backend/fullstackApp/src/test/java/com/cantela/fullstackApp/FullstackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cantela.fullstackApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
