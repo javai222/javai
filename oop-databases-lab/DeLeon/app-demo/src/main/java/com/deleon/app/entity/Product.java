6389d145f96c26abecfdc5df16fd1a16b7edfc11
Lord Ulrick A. De Leon <deleonlua@students.nu-laguna.edu.ph>
2026-05-26 07:11:40 +0000

    Database Task

---
+++ oop-databases-lab/DeLeon/app-demo/src/main/java/com/deleon/app/entity/Product.java
@@ -0,0 +1,60 @@
+package com.deleon.app.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
